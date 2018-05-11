555f575d9b4b1fab46975f20bceafccad53b83f0
Mixmixmix <mikolaj.kundegorski@gmail.com>
2018-05-11 09:42:19 +0300

    runnign notebook

---
+++ training_python_notebook.sh
@@ -0,0 +1,12 @@
+cd ~/training
+ls
+git pull
+#If directory training does not exist do:
+git clone https://github.com/meerkat-code/training.git
+ls
+cd trainig
+ls
+#then:
+cd ~/training/data_storage_solutions/notebook
+ls
+sh docker-commands.sh
